f8e745a7302984e5d6add4341a7723f923bf0616
Nasih P S(UST <284735@ust.com>
2024-11-26 12:26:41 +0530

    added first files

---
+++ app/middleware/cors.py
@@ -0,0 +1,12 @@
+from fastapi.middleware.cors import CORSMiddleware
+
+
+# Enable CORS
+def add_cors_middleware(app):
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=["http://localhost:3000"],
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
